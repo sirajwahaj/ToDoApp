1c4a25a68f5ceaea189e2c8bea4c4c5dad9e479d
Sirajulhaq Wahaj <109559343+sirajwahaj@users.noreply.github.com>
2023-11-05 08:59:59 +0100

    5 routes are add

---
+++ app1.py
@@ -0,0 +1,61 @@
+from flask import Flask, jsonify, request, redirect, render_template, url_for
+import json
+import model
+
+app = Flask(__name__)
+
+
+# GET /tasks Hämtar alla tasks. För VG: lägg till en parameter completed som kan filtrera på färdiga eller ofärdiga tasks.
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+
+# @app.route('/tasks')
+# def tasks():
+#     return jsonify(task1 = "tasks" )
+
+
+# POST /tasks Lägger till en ny task. Tasken är ofärdig när den först läggs till.
+@app.route('/tasks', methods=['POST'])
+def task():
+    task_dic = model.read_form()
+    model.write_tasks_to_file(task_dic)
+    return redirect(url_for('home'))
+
+
+# DELETE /tasks/{task_id} Tar bort en task med ett specifikt id.
+# PUT /tasks/{task_id} Uppdaterar en task med ett specifikt id.
+# GET /tasks/{task_id} Hämtar en task med ett specifikt id.
+@app.route('/tasks/<int:task_id>', methods=['GET', 'PUT', 'DELETE'])
+def get_record(task_id):
+    if request.method == 'GET':
+        return model.get_record(task_id)
+    elif request.method == 'PUT':
+        return model.update_task(task_id)
+        # model.get_record(task_id)
+    elif request.method == 'DELETE':
+        return model.get_record(51)
+
+
+""" 
+@app.route('/tasks/id')
+def update_task():
+    id = int(request.args.get('task_id'))
+    # description = request.args.get('desc')
+    # category = request.args.get('category')
+    # status = request.args.get('status')
+
+    return redirect('/',)
+@app.route('/tasks/<task_id>')
+def update_taskid(task_id):
+    return task_id """
+
+# PUT /tasks/{task_id}/complete Markerar en task som färdig.
+
+# GET /tasks/categories/ Hämtar alla olika kategorier.
+
+# GET /tasks/categories/{category_name} Hämtar alla tasks från en specifik kategori.
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+import json
+
+
+def read_form():
+    task_dic = {}
+    task_dic['id'] = int(request.form['id'])
+    task_dic['description'] = request.form['description']
+    task_dic['category'] = request.form['category']
+    task_dic['status'] = request.form['status']
+    return task_dic
+
+
+def read_tasks_file():
+    try:
+        with open('tasks.json', 'r') as file:
+            file_content = json.load(file)
+            return list(file_content)
+    except json.JSONDecodeError:
+        return []
+
+
+def write_tasks_to_file(dic):
+    data = read_tasks_file()
+    data.append(dic)
+    with open('tasks.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+def get_record(id):
+    data = read_tasks_file()
+    for record in data:
+        if record.get('id') == id:
+            return record
+    return None
+
+
+def update_task(id):
+    updated_record = read_form()
+    orig_record = get_record(id)
+    if orig_record:
+        orig_record.update(updated_record)
+        write_tasks_to_file(orig_record)
+        return jsonify({"message": "Task updated successfully"})
+    else:
+        return False
